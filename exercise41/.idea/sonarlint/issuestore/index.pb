cd0127950f5771a59a365410d2ba0ab36846d8bb
mSpiker1 <mspiker@embarqmail.com>
2021-10-10 19:22:54 -0400

    Added full code without unit tests for exercise 41

---
+++ exercise41/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+L
+docs/plantUML_diagram_41.txt,9\c\9c45dac89a5fb79c5a41df343694ae5df39da73d
+V
+&src/main/java/baseline/Solution41.java,a\7\a7b7fd7319bfb2e7377a9a4572905ac0198ea746
--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -4,43 +4,73 @@
  */
 
 package baseline;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Solution41 {
-    public static void main(String args[]) {
+    public static void main(String[] args) throws FileNotFoundException {
+        //create new object to reference class NameManager
+        NameManager nm = new NameManager();
+
         //create scanner object to read a file
+        Scanner inputFile = new Scanner(new File("data\\exercise41_input.txt"));
 
         //create an array to store the list of names
+        ArrayList<String> nameList = new ArrayList<>();
 
         //call readToList function from nameManager to read the input file to an arrayList of strings
+        nm.readToList(inputFile, nameList);
 
         //sort the current arrayList using sort function
+        nameList.sort(String.CASE_INSENSITIVE_ORDER);
 
         //close input scanner object
+        inputFile.close();
 
         //call writeNames function to create and write the output file from the now sorted arrayList
+        //user try/catch to catch any file not found exception and output to console
+        try{
+            nm.writeNames(nameList);
+        }
+        catch(Exception e){
+            System.out.print("Error: File not found");
+        }
     }
 }
 
 //class containing the functions to manage and sort the list of names
-class nameManager {
+class NameManager {
+
     //function to read through and add lines from input file to an arrayList
-    public void readToList(ArrayList<String>nameList){
+    public void readToList(Scanner input, ArrayList<String>nameList){
         //while loop to loop through lines in the file until there is nothing on the next line
-        //add the current line to the arrayList every loop
+        while(input.hasNextLine()) {
+            //add the current line to the arrayList every loop
+            nameList.add(input.nextLine());
+        }
     }
 
     //function to write output file using sorted list of names
-    public void writeNames(ArrayList<String>sortedNames){
+    public void writeNames(ArrayList<String>sortedNames) throws IOException {
         //create a file writer to write the output file
+        FileWriter writeNames = new FileWriter("data\\exercise41_output.txt");
 
         //write the file header that precedes the sorted list of names
         //"Total of numNames names\n-----------------"
+        writeNames.write("Total of " + sortedNames.size() + " names\n");
+        writeNames.write("-".repeat(17) + "\n");
 
         //for loop to loop through each line until numNames has been reached
         //output the current line each loop
+        for(String i: sortedNames){
+            writeNames.write(i + "\n");
+        }
 
         //close the output file (don't want memory leaks)
+        writeNames.close();
     }
 }
