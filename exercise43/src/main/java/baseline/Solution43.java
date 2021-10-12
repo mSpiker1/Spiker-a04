293cf02cc933cf182c023ac0d9e636786e977e7f
mSpiker1 <mspiker@embarqmail.com>
2021-10-12 14:05:12 -0400

    polished code and added unit testing for exercise43

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -4,58 +4,104 @@
  */
 
 package baseline;
-import java.nio.file.Path;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Solution43 {
     //main method
-    public static void main(String[] args){
-        //create keyboard input scanner, initialize variables for user input
+    public static void main(String[] args) throws IOException {
+        //create class object web to reference class Website
+        Website web = new Website();
+
+        //create keyboard input scanner, initialize variables for user input and a file for base directory
+        Scanner input = new Scanner(System.in);
+        final String siteDir = "data\\website\\";
+        File webDir = new File("data\\website\\");
 
         //ask for website name and assign it to a variable siteName
+        System.out.print("Site name: ");
+        String siteName = input.nextLine();
 
         //ask for author name and assign it to a variable author
+        System.out.print("Author: ");
+        String author = input.nextLine();
 
         //ask if the user wants a folder for JavaScript and assign the answer to String jsyn
+        System.out.print("Do you want a folder for JavaScript? ");
+        String jsyn = input.nextLine();
 
         //ask if the user wants a folder for CSS and assign answer to cssyn
+        System.out.print("Do you want a folder for CSS? ");
+        String cssyn = input.nextLine();
+
+        /*    -create a directory for the website using the File webDir created earlier-
+        I don't know why there is a SonarLint warning here. It says the operation is ignored, but it successfully
+        creates the directory I need when it runs, and the program breaks without it. I've chosen to ignore it and
+        assume that there's either a better way to do this that I don't know about or SonarLint is just being stupid.
+         */
+        webDir.mkdir();
 
         //call createPath to create a path to data/website/siteName
+        boolean baseTest = web.createPath(siteDir + siteName + "\\");
 
-        //call createFile to create an HTML file named index.html using siteName and author for components
+        //if statement to continue the rest of the program if the first directory was created successfully
+        if(baseTest){
 
-        //if jsyn = y || Y
-            //call createPath to create a path at data/website/siteName/js/
+            //call createFile to create an HTML file named index.html using siteName and author for components
+            web.createFile(siteName, author, siteDir + siteName);
 
-        //if cssyn = y || Y
-            //call createPath to create a path at data/website/siteName/css/
+            //if jsyn = y || Y
+            if (jsyn.equals("y") || jsyn.equals("Y")) {
+                //call createPath to create a path at data/website/siteName/js/
+                web.createPath(siteDir + siteName + "\\js\\");
+            }
+
+            //if cssyn = y || Y
+            if (cssyn.equals("y") || cssyn.equals("Y")) {
+                //call createPath to create a path at data/website/siteName/css/
+                web.createPath(siteDir + siteName + "\\css\\");
+            }
+        }else{
+            //prints this to console if the first directory was not successfully created
+            System.out.print("The directory could not be created.");
+        }
     }
 }
 
 //class to handle creation/adjustment of website files/directories
 class Website{
     //method to create a directory at filePath
-    void createPath(String filePath){
-        //initialize File variable "directory" at filePath
+    boolean createPath(String filePath){
+        //initialize File variable "directory" at filePath, also initialize boolean variable to return for a unit test
+        File directory = new File(filePath);
+        boolean success;
 
-        //create the directory at filePath using mkdir method
+        //create the directory at filePath using mkdir method, also set success to true if the creation was successful
+        success = directory.mkdir();
 
         //output "created [directory]"
+        System.out.printf("Created %s%n", filePath);
+
+        //return the value of success
+        return success;
     }
 
     //method to create an HTML file at filePath
-    void createFile(String siteName, String author, String filePath){
+    void createFile(String siteName, String author, String filePath) throws IOException {
         //create fileWriter "index" at filePath
+        try (FileWriter htmlWriter = new FileWriter(filePath + "\\index.html")) {
 
-        //sequence to write html file:
-        //write <html>
-        //write <head>
-        //write <meta name = "author" content = "%s">, author
-        //write </head>
-        //write <title>
-        //write siteName
-        //write </title>
-        //write <body>
+            //sequence to write html file:
+            htmlWriter.write("<html>\n<head>\n");
+            htmlWriter.write("<meta name = \"author\" content = \"" + author + "\">"); //write the author name in a meta tag
+            htmlWriter.write("\n</head>\n<title>\n");
+            htmlWriter.write(siteName); //write the site name in the title tag
+            htmlWriter.write("\n</title>\n<body>");
+        }
 
         //output "created [filedir]"
+        System.out.printf("created %s\\index.html%n", filePath);
     }
 }
--- exercise43/src/test/java/baseline/WebsiteTest.java
+++ exercise43/src/test/java/baseline/WebsiteTest.java
@@ -1,4 +1,70 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class WebsiteTest {
-  
+    @Test
+    //tests createPath method
+    void testCreate(){
+        //class reference object for Website()
+        Website web = new Website();
+
+        //create File object with the address of a test folder to be created (called "test")
+        File testPath = new File("test\\");
+
+        //create string with the address of the test folder written to it
+        String filePath = "test\\";
+
+        //create a boolean value to be used as a testing variable, defaulted to false
+        boolean testValue = false;
+
+        //run the createPath method and set testValue to what it returns (should return true if it is successful)
+        testValue = web.createPath(filePath);
+
+        //delete the test folder directory
+        testPath.delete();
+
+        //test if the testValue variable has been set to true, as expected
+        assertTrue(testValue);
+    }
+
+    @Test
+    void testFile() throws IOException {
+        //class reference object for Website
+        Website web = new Website();
+
+        //create file objects for test directory and the html file to be created
+        File path = new File("testHTML\\");
+        File htmlPath = new File("testHTML\\index.html");
+
+        //create string with test directory address written to it
+        String filePath = "testHTML\\";
+
+        //creates the test directory
+        path.mkdir();
+
+        //creates the HTML file inside the test directory using the createFile method
+        web.createFile("Name", "Author", filePath);
+
+        //create expected String values of lines 2 and 5 of the html file that was created by createFile()
+        String expected2 = "<meta name = \"author\" content = \"Author\">";
+        String expected5 = "Name";
+
+        //create Strings equal to the values found on lines 2 and 5 of the html file
+        String line2 = Files.readAllLines(Paths.get("testHTML\\index.html")).get(2);
+        String line5 = Files.readAllLines(Paths.get("testHTML\\index.html")).get(5);
+
+        //deletes the test directory
+        htmlPath.delete();
+        path.delete();
+
+        assertEquals(line2, expected2);
+        assertEquals(line5, expected5);
+    }
 }
